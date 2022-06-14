66218a50d357e9a6cbbf96ccc3e62314e90964c5
Yuchen Chen <angelchen3311@gmail.com>
2022-06-14 16:14:16 -0400

    commit tree eab7baa8b2d0077c7ac3f2955b6326f6c5298589

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph);
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat(Ralph){
+    cats.pop(Ralph);
+}
+
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo);
+}
+
+const copyOfCats = ["Broom"];
+function appendCat(){
+    let copyOfCats = ([...cats, "Broom"]);
+    return copyOfCats
+}
+
+const copyOfCats1 = ["Arnold"];
+function prependCat(){
+    let copyOfCats1 = (["Arnold", ...cats]);
+    return copyOfCats1
+}
+
+function removeLastCat(){
+    const cats1 = cats.slice(0, cats.length -1);
+    return cats1;
+}
+
+function removeFirstCat(){
+    const cats2 = cats.slice(1);
+        return cats2;
+}
